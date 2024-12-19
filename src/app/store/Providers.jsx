d58b8dcf73b1b15f7ada3f58489f33cf64b0bfa0
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2024-12-19 14:18:31 +0800

    style: declare classnames for theme switching, and suppressHydrationWarning to remove errors

--- src/app/store/Providers.jsx
+++ src/app/store/Providers.jsx
@@ -3,9 +3,17 @@ import React from "react";
 import { ThemeProvider } from "next-themes";
 
 const Providers = ({ children }) => {
+  const className =
+    "text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300 ease-in-out";
+
   return (
-    <ThemeProvider defaultTheme="system" attribute="class">
-      <div className="">{children}</div>
+    <ThemeProvider
+      defaultTheme="system"
+      attribute="class"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className={className}>{children}</div>
     </ThemeProvider>
   );
 };
--- src/components/Header/components/ThemeSwitch.jsx
+++ src/components/Header/components/ThemeSwitch.jsx
@@ -1,22 +1,28 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 import { useTheme } from "next-themes";
 
 const ThemeSwitch = () => {
   const { theme, setTheme, systemTheme } = useTheme();
-
+  const [mounted, setMounted] = useState(false);
   const currentTheme = theme === "system" ? systemTheme : theme;
+
+  useEffect(() => setMounted(true), []);
 
   const iconClass = "text-xl cursor-pointer hover:text-amber-500";
 
   return (
     <div className="cursor-pointer">
-      {currentTheme === "dark" ? (
-        <MdLightMode onClick={() => setTheme("light")} className={iconClass} />
-      ) : (
-        <MdDarkMode onClick={() => setTheme("dark")} className={iconClass} />
-      )}
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            onClick={() => setTheme("light")}
+            className={iconClass}
+          />
+        ) : (
+          <MdDarkMode onClick={() => setTheme("dark")} className={iconClass} />
+        ))}
     </div>
   );
 };
